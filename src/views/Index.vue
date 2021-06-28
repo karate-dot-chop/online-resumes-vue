c12a6ce07727f523885acfba34db147e1af62b64
ryan <ball.ryan.rb@gmail.com>
2021-06-28 13:32:57 -0600

    updates App.vue header, adds index.vue page and route

---
+++ src/views/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+// import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Here is a list of all students!",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
