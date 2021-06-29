642f9178ef794bbca209ad264fbf663ba90fb20f
ryan <ball.ryan.rb@gmail.com>
2021-06-29 12:16:58 -0600

    adds tiles for students index

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="index">
-    <h1>{{ message }}</h1>
+    <div class="inner">
+      <h1>{{ message }}</h1>
+      <section class="tiles">
+        <article
+          class="style7"
+          v-for="student in students"
+          v-bind:key="student.id"
+        >
+          <span class="image">
+            <img :src="student.photo_url" alt="" />
+          </span>
+          <!-- <router-link> -->
+          <!-- ADD LINK TO SHOW PAGE IN ROUTER-LINK - USE STUDENT.ID -->
+          <a>
+            <h2>
+              {{ student.first_name }} <br />
+              {{ student.last_name }}
+            </h2>
+            <div class="content">
+              <p>
+                {{ student.email }} <br />
+                <i>{{ student.bio }}</i>
+              </p>
+            </div>
+          </a>
+          <!-- </router-link> -->
+        </article>
+      </section>
+    </div>
+    <br />
   </div>
 </template>
 
