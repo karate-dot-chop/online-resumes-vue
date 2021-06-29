d60e4232f0abb3ad43c8856918de9c11a734c954
ashley <agahagan1@gmail.com>
2021-06-29 16:13:23 -0400

    adds twitter and changes footer

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
 import TwitterFeed from "vuejs-twitter-feed";
 Vue.use(TwitterFeed);
-import axios from "axios";
 
-axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 Vue.config.productionTip = false;
 
--- src/views/Twitter.vue
+++ src/views/Twitter.vue
@@ -1,15 +1,5 @@
 <template>
   <div>
-    <a
-      class="twitter-timeline"
-      data-width="300"
-      data-height="400"
-      href="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw"
-    >
-      Tweets by TwitterDev
-    <src="https://platform.twitter.com/widgets.js" charset="utf-8"
-    </div>
+    <TwitterFeed src="https://twitter.com/potus?ref_src=twsrc%5Etfw" data-width="300" data-height="400"></TwitterFeed>
+  </div>
 </template>
-
-//
-<TwitterFeed src="https://twitter.com/vuejs?ref_src=twsrc%5Etfw" charset="utf-8"></TwitterFeed>
